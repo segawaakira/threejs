2584c1b048a9e411e5afceeed910cc43a84f8d35
segawa <>
2025-07-28 16:36:10 +0900

    feat: passwordをhash化

--- apps/api/src/users/users.service.ts
+++ apps/api/src/users/users.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 // Simplicity - Just a simple service to create and list users
 @Injectable()
@@ -7,8 +8,9 @@ export class UsersService {
   constructor(private readonly prisma: PrismaService) {}
 
   async createUser(email: string, password: string) {
+    const hashedPassword = await bcrypt.hash(password, 10);
     const user = await this.prisma.user.create({
-      data: { email, password },
+      data: { email, password: hashedPassword },
     });
 
     return user;
--- apps/web/components/create-user.tsx
+++ apps/web/components/create-user.tsx
@@ -15,7 +15,7 @@ const CreateUser = () => {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        email: "example@example.com",
+        email: "example+2@example.com",
         password: "example",
       }),
     });
