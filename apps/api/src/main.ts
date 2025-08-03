dd719bbd099184cd4912597e02c5396eca8707e5
segawa <>
2025-08-03 14:02:27 +0900

    Update main.ts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,6 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  // app.enableCors({
+  //   origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+  //   credentials: true,
+  //   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  //   allowedHeaders: ['Content-Type', 'Authorization'],
+  // });
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
